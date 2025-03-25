4a36a4b37a64e6b65474a6079e55433e77607890
Masudur Rahman <45783647+chintitomasud3@users.noreply.github.com>
2025-03-25 15:25:25 +0600

    Create url.py

---
+++ needto/url.py
@@ -0,0 +1,2 @@
+What is a Uniform Resource Locator (URL)?
+A Uniform Resource Locator (URL) is a reference or address used to locate resources on the internet, such as web pages, files, or services. It’s a standardized way to identify the location of a resource and specify how to access it (e.g., via HTTP, HTTPS, FTP).
